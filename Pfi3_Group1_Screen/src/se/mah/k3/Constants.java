93aa6abdccfe82f0992388d53679a0197eed11c4
Jesper Andersson <jsprA86@gmail.com>
2015-05-11 23:20:20 +0200

    Added comments, increased readability.

--- Pfi3_Group1_Screen/src/se/mah/k3/Constants.java
+++ Pfi3_Group1_Screen/src/se/mah/k3/Constants.java
@@ -4,5 +4,4 @@ public class Constants {
 	
 	//public static final String FIREBASE_URL = "https://blinding-torch-9917.firebaseio.com/";
 	public static final String FIREBASE_URL = "https://boiling-heat-8172.firebaseio.com/";
-	
 }
--- Pfi3_Group1_Screen/src/se/mah/k3/Logic.java
+++ Pfi3_Group1_Screen/src/se/mah/k3/Logic.java
@@ -11,33 +11,39 @@ public class Logic {
 	
 	private static final long serialVersionUID = 1L;
 	
-	ArrayList<TreasureLocation> treasureLocations = new ArrayList<TreasureLocation>();	
-	Firebase firebase;
+	private ArrayList<TreasureLocation> treasureLocations = new ArrayList<TreasureLocation>();	
+	private Firebase firebase;
 	
 	
 	public Logic() {
 		
-		addLocations();		
-		for (TreasureLocation tl : treasureLocations){
-			System.out.print(tl.getId()+" type: "+tl.getType()+" at: "+tl.getPosX()+" "+tl.getPosY()+"\n");
-		}
-		
-		
-		firebase = new Firebase(Constants.FIREBASE_URL);
-		firebase.removeValue();
-		//firebase.child("Test").setValue("Do you have data?");
-		
-		
-		
-		
-		//Thread.sleep(3000);
+		setupTreasureLocations();
+		setupFirebase();
+			
 	}
 	
-	public void addLocations(){
-		treasureLocations.add(new TreasureLocation("TL101",10,10));
-		treasureLocations.add(new TreasureLocation("TL102",20,20));
-		treasureLocations.add(new TreasureLocation("TL103",50,50));
-		treasureLocations.add(new TreasureLocation("TL104",100,100));
-		treasureLocations.add(new TreasureLocation("TL105",300,100));
+	//Add all treasure locations to the treasureLocations array. If we want more treasure locations we add them here
+	public void setupTreasureLocations(){
+		
+		//Every added new TreasureLocation is initialized with id, x position and y position
+		treasureLocations.add(new TreasureLocation("TL01",10,10));
+		treasureLocations.add(new TreasureLocation("TL02",20,20));
+		treasureLocations.add(new TreasureLocation("TL03",50,50));
+		treasureLocations.add(new TreasureLocation("TL04",100,100));
+		treasureLocations.add(new TreasureLocation("TL05",300,100));
 	}
+	
+	//Create a referens to firebase and sets all default values. Run in the start of a program.
+	public void setupFirebase(){			
+		
+		firebase = new Firebase(Constants.FIREBASE_URL); //Instantiating the firebase referens with our firebase url
+		firebase.removeValue(); //Empties the whole firebase app
+		firebase.child("LightCue").setValue(0);//Create the LightCue child	
+		
+		//Create one child for each member of the treasureLocations array and sets the child active to 0
+		for (TreasureLocation tl : treasureLocations){
+			firebase.child(tl.getId()+"/active").setValue(0);
+		}
+	}
+	
 }
